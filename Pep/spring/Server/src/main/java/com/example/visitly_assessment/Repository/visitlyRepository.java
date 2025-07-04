6bc858314720e5a33e8d0923ee0a786f05e200c8
Vignesh S <116181639+Vignesh-S-18-2003@users.noreply.github.com>
2025-07-05 01:38:00 +0530

    Delete Pep/spring/Server directory

--- Pep/spring/Server/src/main/java/com/example/visitly_assessment/Repository/visitlyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.visitly_assessment.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.visitly_assessment.model.visitlyModel;
-
-public interface visitlyRepository extends JpaRepository<visitlyModel, Long> {
-
-}
